f8cc667247a7153c8d10f514101270de3aa3ab04
chiraant <chiraant@students.zhaw.ch>
2024-04-13 15:01:44 +0200

    Refactor AssetController to include getAllAsset endpoint closes #6

--- src/main/java/ch/zhaw/myclient/controller/AssetController.java
+++ src/main/java/ch/zhaw/myclient/controller/AssetController.java
@@ -1,10 +1,11 @@
 package ch.zhaw.myclient.controller;
 
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,18 +25,17 @@ public class AssetController {
 
     @PostMapping("/asset")
     public ResponseEntity<Asset> createAsset(
-        @RequestBody AssetCreateDTO aDTO) {
+            @RequestBody AssetCreateDTO aDTO) {
         Asset aDAO = new Asset(aDTO.getAssetName(), aDTO.getPurchaseDate(), aDTO.getOwner(), aDTO.getGuaranteeEnd());
         Asset a = assetRepository.save(aDAO);
-        
+
         return new ResponseEntity<>(a, HttpStatus.CREATED);
     }
+
+    @GetMapping("/asset")
+    public ResponseEntity<List<Asset>>getAllAsset() {
+        List<Asset> allAsset = assetRepository.findAll();
+        return new ResponseEntity<>(allAsset, HttpStatus.OK);
+    }
     
-    
-
-
-
-
-
-
 }
